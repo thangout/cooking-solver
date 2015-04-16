6ad7943423b1e01ba29b7770548772a1c8d99f18
dpadmin@seznam.cz <Do>
2015-04-16 17:02:08 +0200

    Parsing ingredients from file

--- src/cookingsolver/CookingSolver.java
+++ src/cookingsolver/CookingSolver.java
@@ -3,11 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package cookingsolver;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Random;
 
 /**
  *
@@ -15,36 +17,34 @@ import java.util.Random;
  */
 public class CookingSolver {
 
+	static ArrayList<Ingredient> ingredients; 
 	/**
 	 * @param args the command line arguments
 	 */
-	public static void main(String[] args) {
-		String[] ingredientsName = {"testoviny","hovezi maso","rajce","cibule","okurka","chleba","vajicko"};
-		ArrayList<Ingredient> ingredients = new ArrayList<>();
-		ArrayList<Recipe> recipes = new ArrayList<>();
-		Random rnd = new Random(5);
-		for (int i = 0; i < ingredientsName.length; i++) {
-			int price = (int) (rnd.nextDouble()*500+1);
-			int weight = (int) (rnd.nextDouble()*1000+1);
-			Ingredient temp = new Ingredient(ingredientsName[i],weight,price);
-			ingredients.add(temp);
-		}
-
-		for (int i = 0; i < 5; i++) {
-			Recipe temp = new Recipe();
-			int randomIndex = (int) (rnd.nextDouble()*ingredients.size());
-			for (int j = 0; j < (int)(rnd.nextDouble()*100 + 1); j++) {
-				temp.addIngredient(ingredients.get(randomIndex),100);
-			}
-			recipes.add(temp);
-		}
-		
-		for (int i = 0; i < recipes.size(); i++) {
-//			System.out.println(recipes.get(i).getPrice());	
-		}
-		Solver solver = new Solver(ingredients);
-//		System.out.println(solver.getRecipeSize());
-		solver.printRecipes();
+	public static void main(String[] args) throws IOException {
+		ingredients = new ArrayList<>();
+		parseIngredients();
 	}
-	
+
+	public static void parseIngredients() throws IOException{
+		FileInputStream fstream = new FileInputStream("ingredients.txt");
+		BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+		String line = "";
+		Recipe recp = new Recipe();
+		while ((line = br.readLine()) != null) {
+			if ("#".equals(line)) {
+				break;		
+			}else{
+				String[] temp = line.trim().split(",");
+				String name = temp[0]; 
+//				System.out.println(name);
+				String[] values = temp[1].trim().split(" ");
+				int weight = Integer.parseInt(values[0]);
+				int price  = Integer.parseInt(values[1]);
+				Ingredient ing = new Ingredient(name,weight,price);
+				ingredients.add(ing);
+				
+			}
+		}
+	}
 }
