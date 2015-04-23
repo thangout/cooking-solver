e49982bcda1dbee16e4fc8755b28c974408579fc
Thang Do <pandothang@gmail.com>
2015-04-24 00:23:51 +0200

    Implement sum of weight of recipe

--- src/cookingsolver/Recipe.java
+++ src/cookingsolver/Recipe.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package cookingsolver;
 
 import java.util.ArrayList;
@@ -18,46 +17,55 @@ import java.util.Set;
  * @author Thang Do
  */
 public class Recipe {
+
 	HashMap usage;
 	String name;
 
 	public Recipe(String name) {
 		this.name = name;
-		usage = new HashMap<Ingredient,Integer>();
+		//<Ingredient, weight usage>
+		usage = new HashMap<Ingredient, Integer>();
 	}
 
-	public void addIngredient(Ingredient ing, int amount){
+	public void addIngredient(Ingredient ing, int amount) {
 		usage.put(ing, amount);
 	}
 
-	public void removeIngredient(Ingredient ing ){
+	public void removeIngredient(Ingredient ing) {
 		usage.remove(ing);
 	}
-	
-	public float getPrice(){
-		Set<Entry<Ingredient,Integer>> entrySet = usage.entrySet();
+
+	public float getPrice() {
+		Set<Entry<Ingredient, Integer>> entrySet = usage.entrySet();
 		float price = 0;
-		Iterator ite = entrySet.iterator();
-		while(ite.hasNext()){
-			Entry<Ingredient,Integer> temp = (Entry<Ingredient,Integer>) ite.next();
-			price += (temp.getKey().getPrice() * (float)temp.getValue());
+		for (Entry<Ingredient, Integer> temp : entrySet) {
+			price += (temp.getKey().getPrice() * (float) temp.getValue());
 		}
-	return price;	
+		return price;
 	}
 
-	public void printIngredients(){
-		Set<Entry<Ingredient,Integer>> entrySet = usage.entrySet();
+	public int getWeight() {
+		Set<Entry<Ingredient, Integer>> entrySet = usage.entrySet();
+		int weight = 0;
+		for (Entry<Ingredient, Integer> temp : entrySet) {
+			weight += temp.getValue();
+		}
+		return weight;
+	}
+
+	public void printIngredients() {
+		Set<Entry<Ingredient, Integer>> entrySet = usage.entrySet();
 		Iterator ite = entrySet.iterator();
 		System.out.println("______________");
 		System.out.println(name);
-		while(ite.hasNext()){
-			Entry<Ingredient,Integer> temp = (Entry<Ingredient,Integer>) ite.next();
+		while (ite.hasNext()) {
+			Entry<Ingredient, Integer> temp = (Entry<Ingredient, Integer>) ite.next();
 			System.out.println(temp.getKey().getName() + " : " + temp.getValue() + "g");
 		}
 		System.out.println("Price: " + getPrice() + ",-");
 	}
 
-	public HashMap getIngredientsNeeded(){
+	public HashMap getIngredientsNeeded() {
 		return usage;
 	}
 }
